ab4cb488e2fefda4e1cc688d85db1951e5b44762
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2024-01-09 11:23:02 +0530

    while loop code is added

--- src/main/java/com/example/DemoLoops.java
+++ src/main/java/com/example/DemoLoops.java
@@ -1,14 +1,28 @@
 package main.java.com.example;
 
 public class DemoLoops {
-//    java provided following looping statements.
-/* imp: while loop and so while loop
-* for loop and foreach loop
-* to execute the same code repeatedly
-* //note:
-* */
+// java provided following looping statements.
+//note:
+// while loop and so while loop  - condition based
+// * for loop and foreach loop - interaction based
+// * to execute the same code repeatedly.
+
+
+//Syntax:
+//    while(condition){
+//        stmts;
+//    }
+    /* * while loop used to run the same repeatedly as long as the given statement is satisfied */
 
     public static void main(String[] args) {
+        int i = 1;
+        while (i<=100){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("value end of while loop:"+i);
+
+        System.out.println("end the while loop");
 
     }
 }
