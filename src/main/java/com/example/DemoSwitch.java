9784648589a1585749ea1fbee5c54e0854e05256
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2024-01-09 10:53:19 +0530

    I have added the switch case in the files

--- src/main/java/com/example/DemoSwitch.java
+++ src/main/java/com/example/DemoSwitch.java
@@ -1,7 +1,49 @@
 package main.java.com.example;
 
+import java.util.Scanner;
+
 public class DemoSwitch {
+
+    // TODO: Switch case
+    /*
+     * it is also used to check multiple conditions
+     * if you want to check multiple conditions instead of implement a complex nested if better to go with with switch case
+     *
+     * //Syntax:
+     *  switch (variable) {
+            case value:
+                stmts;
+                stmts2;
+                break;
+            case value2:
+                stmts;
+                stms;
+            default:
+                stmsts;
+
+        }
+
+     * */
     public static void main(String[] args) {
 
+        System.out.println("Enter Course Code");
+        Scanner sc = new Scanner(System.in);
+        String courseCode = sc.nextLine();
+        switch (courseCode.toLowerCase()){
+            case "j":
+                System.out.println("java");
+                break;
+            case "p":
+                System.out.println("python");
+                break;
+            case "t":
+                System.out.println("Testing");
+                break;
+            default:
+                System.out.println("Course doesn't exits");
+        }
+
+
     }
+
 }
