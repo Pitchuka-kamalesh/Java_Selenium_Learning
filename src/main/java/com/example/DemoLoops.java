8d04b3f9ad336d0a908d1ba9b81e370cc2899522
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2024-01-09 11:10:11 +0530

    I have added the switch case in the files and added the looping files

---
+++ src/main/java/com/example/DemoLoops.java
@@ -0,0 +1,14 @@
+package main.java.com.example;
+
+public class DemoLoops {
+//    java provided following looping statements.
+/* imp: while loop and so while loop
+* for loop and foreach loop
+* to execute the same code repeatedly
+* //note:
+* */
+
+    public static void main(String[] args) {
+
+    }
+}
