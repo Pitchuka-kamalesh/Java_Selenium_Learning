cefc4267f5f0b6aee0b2e0f01517c294b96209fd
Pitchuka-kamalesh <kamalesh.4025@gmail.com>
2024-01-10 10:19:39 +0530

    while loop code is added

---
+++ src/main/java/com/example/Calendar.java
@@ -0,0 +1,50 @@
+package main.java.com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class Calendar {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver;
+
+        By clicks = By.xpath("//a[@title='Next']");
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\kamal\\eclipse-workspace\\MyProject\\src\\Resources\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://jqueryui.com/datepicker/");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(5,TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(5,TimeUnit.SECONDS);
+
+//
+        WebElement frameI = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
+        driver.switchTo().frame(frameI);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.id("datepicker")).click();
+        WebElement nextButton = driver.findElement(clicks);
+        do {
+
+            try {
+                nextButton.click();
+            }catch (org.openqa.selenium.StaleElementReferenceException e){
+                System.out.println(e);
+                Thread.sleep(500);
+                driver.findElement(clicks).click();
+                Thread.sleep(500);
+
+            }
+
+        }while(driver.findElement(clicks).isEnabled());
+
+
+
+
+    }
+}
--- src/main/java/com/example/DemoLoops.java
+++ src/main/java/com/example/DemoLoops.java
@@ -13,16 +13,33 @@ public class DemoLoops {
 //        stmts;
 //    }
     /* * while loop used to run the same repeatedly as long as the given statement is satisfied */
+//    do-while loop first excite the statements after executing the statement one time verify the  condition.
+//syntax:
+// do {
+//     System.out.println(j);
+//      j++;
+//     }while (j<=100);
+
+//note: do loop  first excuite the statement and later verify the condition to continue.
+
 
     public static void main(String[] args) {
-        int i = 1;
-        while (i<=100){
-            System.out.println(i);
-            i++;
-        }
-        System.out.println("value end of while loop:"+i);
-
+//        int i = 1;
+//        while (i<=100){
+//            System.out.println(i);
+//            i++;
+//        }
+//        System.out.println("value end of while loop:"+i);
+//
+//        System.out.println("end the while loop");
+        //example2: do-while loop
+        int j =1;
+        do {
+            System.out.println(j);
+            j++;
+        }while (j>=100);
         System.out.println("end the while loop");
+
 
     }
 }
